f2104dafd4ead0b345da2a5a552219e865853e4f
Fiona Young <zcqsfey@ucl.ac.uk>
2019-10-31 11:17:37 +0000

    created 2 test files that pass at time of commit

---
+++ 05-testing/test_class_times.py
@@ -0,0 +1,8 @@
+from times import *
+
+def test_given_input():
+    large = time_range("2019-10-31 10:00:00", "2019-10-31 13:00:00")
+    short = time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 600)
+    result = overlap_time(large,short) 
+    expected = short
+    assert result == expected
