89c377250965fae791729b3db2fad8e05ab8a5e0
Fiona Young <zcqsfey@ucl.ac.uk>
2019-10-31 11:21:24 +0000

    Add comments Answers UCL-RITS/rsd-classwork-2019#25

--- 05-testing/test_class_times.py
+++ 05-testing/test_class_times.py
@@ -1,8 +1,11 @@
 from times import *
 
 def test_given_input():
+    # Create two datetime ranges
     large = time_range("2019-10-31 10:00:00", "2019-10-31 13:00:00")
     short = time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 600)
+    # Compute overlap
     result = overlap_time(large,short) 
+    # result should be the same as short
     expected = short
     assert result == expected
